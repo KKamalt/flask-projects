2f35234600d78743b037f3c799bcaf458ac4c893
KKamalt <kamalteja2k@gmail.com>
2023-11-24 01:47:28 +0530

    Removed unecessary things

--- Ecommerce-Website/app.py
+++ Ecommerce-Website/app.py
@@ -1,6 +1,4 @@
-from email.mime import image
-from itertools import product
-from typing import Any
+# from itertools import product
 from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 import sqlalchemy
@@ -14,13 +12,6 @@ class Product(db.Model):
     name = db.Column(db.String(200), nullable=False)
     price = db.Column(db.Float, nullable=False)
     product = db.Column(db.String(200),nullable=False)
-# # Sample product data (in a real application, this would come from a database)
-# products = [
-#     {"id": 1, "name": "Apple", "price": 10.0,  "product":"apple.jpg" },
-#     {"id": 2, "name": "Banana", "price": 15.0, "product":"banana.jpg"},
-#     {"id": 3, "name": "Grapes", "price": 20.0, "product":"grapes.jpg"},
-# ]
-
 
 with app.app_context():
     db.create_all()
